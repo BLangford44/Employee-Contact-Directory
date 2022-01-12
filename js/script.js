509221a35617b482f1c338886d1b62536be471cc
BLangford44 <92070175+BLangford44@users.noreply.github.com>
2022-01-12 12:11:20 -0600

    Refactoring
    Finalized CSS/HTML.  Added comments to JavaScript.

--- js/script.js
+++ js/script.js
@@ -1,59 +1,55 @@
-let employees = [];
-const url = 'https://randomuser.me/api/?results=12'
+let employees = []; //Empty array to store employee data
+const url = 'https://randomuser.me/api/?results=12' // URL for fetch
 
 // Fetch Data
 const data = fetch(url)
-  .then(response => response.json())
-  .then(response => response.results)
-  .then(generateEmployeeCard)
-  .catch(err => console.log(err));
+  .then(response => response.json()) // parse response
+  .then(response => response.results) // Get results of fetch from object
+  .then(generateEmployeeCard) // Use results to generate employee cards
+  .catch(err => console.log(err)); // Print error message to console
 
 const grid = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
 const modalClose = document.querySelector('.modal-close');
 
+/**
+ * Function: Will generate/set html for the grid-container in the Index file
+ * 
+ * @param  {array} data = Array containing employee objects
+ */
+
 function generateEmployeeCard(data) {
+
+  let gridHtml = '';
 
   data.forEach((employee) => {
 
-    // Card Div
-    const div = document.createElement('div')
-    div.className = 'card';
-    div.setAttribute('data-index', `${data.indexOf(employee)}`);
+    const cardHtml = `
+            <div class='card' data-index='${data.indexOf(employee)}'>
+              <img class='avatar' src='${employee.picture.medium}'>
+              <div class='text-container'>
+                <h2 class="name">${employee.name.first} ${employee.name.last}</h2>
+                <p class="email">${employee.email}</p>
+                <p class="address">${employee.location.city}, ${employee.location.state}</p>
+              </div>
+            </div>
+    `
 
-    // Image Element
-    const img = document.createElement('img');
-    img.className = 'avatar';
-    img.src = `${employee.picture.medium}`;
-
-    // Text Div
-    const infoDiv = document.createElement('div');
-    infoDiv.className = 'text-container';
-
-    const h2 = document.createElement('h2');
-    h2.className = 'name';
-    h2.textContent = `${employee.name.first} ${employee.name.last}`;
-
-    // Add Email
-    const email = document.createElement('p');
-    email.className = 'email';
-    email.textContent = `${employee.email}`;
-
-    // Add Location
-    const location = document.createElement('p');
-    location.textContent = `${employee.location.city}, ${employee.location.state}`;
-    location.className = 'address';
-
-    infoDiv.append(h2, email, location)
-    div.append(img, infoDiv)
-
-    grid.appendChild(div);
+    gridHtml += cardHtml; //Add Card HTML to Grid Variable
 
     // Store Employee Information
     employees.push(employee);
-  });
+  }); // End forEach Loop
+
+  grid.innerHTML = gridHtml;
 }
+
+/**
+ * Function: Will generate/set the html for the modal-container conatained in the Index File
+ * 
+ * @param  {number} index = selected card's 'data-index' value
+ */
 
 function displayModal(index) {
 
@@ -61,7 +57,6 @@ function displayModal(index) {
   let birthday = new Date(dob.date);
 
   const modalHTML = `
-<div class="modal-content">
           <img src="${picture.large}" alt="Employee photo" class="avatar">
           <div class="text-container">
             <h2 class="name">${name.first} ${name.last}</h2>
@@ -72,16 +67,20 @@ function displayModal(index) {
             <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
             <p class='birthday'>Birthday: ${birthday.getMonth()}/${birthday.getDate()}/${birthday.getFullYear()}</p>
           </div>
-        </div>
 `;
 
   overlay.classList.remove('hidden');
   modalContainer.innerHTML = modalHTML;
 }
 
+/**
+Click Event:
+
+- Will dislay modal when Employee's Card is clicked
+
+ */
 
 grid.addEventListener('click', (e) => {
-
   if (e.target !== grid) {
 
     const card = e.target.closest('.card');
@@ -90,5 +89,12 @@ grid.addEventListener('click', (e) => {
     displayModal(index);
   }
 });
+
+/**
+Click Event:
+
+- Close modal with button click
+
+ */
 
 modalClose.addEventListener('click', () => overlay.classList.add('hidden'));
